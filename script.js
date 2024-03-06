a341e24082daf8cf6d9594b3ab528414e7c2a932
TJ <t.jenya11@gmail.com>
2024-03-07 01:38:25 +0200

    Added move displayer and delay inbetween rounds. Made final touches: bg, fonts, font-sizes and cleaned up js

--- script.js
+++ script.js
@@ -10,6 +10,17 @@ const divWinner = document.querySelector(".winner");
 const WinnerText = document.querySelector(".winner > h2");
 const roundCounter = document.querySelector(".round-status > h2");
 const drawText = document.querySelector(".round-status > h3");
+const game = document.querySelector(".game");
+const divCompMove = document.querySelector(".comp-move");
+const divPlayerMove = document.querySelector(".player-move");
+
+const rockIcon = document.querySelector(".rock-mini-comp");
+const paperIcon = document.querySelector(".paper-mini-comp");
+const scissorsIcon = document.querySelector(".scissors-mini-comp");
+
+const rockIconP = document.querySelector(".rock-mini-player");
+const paperIconP = document.querySelector(".paper-mini-player");
+const scissorsIconP = document.querySelector(".scissors-mini-player");
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -32,21 +43,44 @@ function getcomputer(){
         return "paper"
     }
     else if (number === 3){
-        return "scisors"
+        return "scissors"
     }
 }
 
 async function play(move, computerScoreHolder, playerScoreHolder){
-    drawText.textContent = "";
+    btnsMoves.forEach((button) => button.disabled = true);
 
     let player = move;
     let computer = getcomputer();
+
+    if (player === "rock"){
+        rockIconP.style.color = "#a8ffbf";
+    }
+    else if (player === "paper"){
+        paperIconP.style.color = "#a8ffbf";
+    }
+    else if (player === "scissors"){
+        scissorsIconP.style.color = "#a8ffbf";
+    }
+
+    if (computer === "rock"){
+        rockIcon.style.color = "#a8ffbf";
+    }
+    else if (computer === "paper"){
+        paperIcon.style.color = "#a8ffbf";
+    }
+    else if (computer === "scissors"){
+        scissorsIcon.style.color = "#a8ffbf";
+    }
+    
     if(player !== computer){
 
         if (player === "rock" && computer == "paper"){
             computerScoreHolder.textContent = Number(computerScoreHolder.textContent) + 1;
+            
+
         }
-        else if (player === "rock" && computer == "scisors"){
+        else if (player === "rock" && computer == "scissors"){
             playerScoreHolder.textContent = Number(playerScoreHolder.textContent) + 1;
         }
 
@@ -54,72 +88,78 @@ async function play(move, computerScoreHolder, playerScoreHolder){
             playerScoreHolder.textContent = Number(playerScoreHolder.textContent) + 1;
 
         }
-        else if (player === "paper" && computer == "scisors"){
+        else if (player === "paper" && computer == "scissors"){
             computerScoreHolder.textContent = Number(computerScoreHolder.textContent) + 1;
 
         }
 
-        else if (player === "scisors" && computer == "paper"){
+        else if (player === "scissors" && computer == "paper"){
             playerScoreHolder.textContent = Number(playerScoreHolder.textContent) + 1;
 
         }
-        else if (player === "scisors" && computer == "rock"){
+        else if (player === "scissors" && computer == "rock"){
             computerScoreHolder.textContent = Number(computerScoreHolder.textContent) + 1;
-        }
-
-        if (currentRound >= rounds){
-            console.log("EEEEEEEEEENNNNNNNNNNNNNNNNNNNNNNNNNNNDDDDDDDDDDDDDDDDDDDDDDDDD");
-            winner = Number(computerScoreHolder.textContent) > Number(playerScoreHolder.textContent) ? 
-            "computer" : Number(computerScoreHolder.textContent) === Number(playerScoreHolder.textContent)?
-            "draw" : playerName;
-            endMatch(winner);
-            
-        }
-        else{
-            currentRound++;
-            roundCounter.textContent = currentRound + "/" + rounds;
-
+        
         }
 
 
         
     }
     else{
-        drawText.textContent = "Draw";        
+        drawText.textContent = "Draw";       
     }
+
+    await sleep(1200);
+
+    rockIconP.style.color = "#000000";
+    paperIconP.style.color = "#000000";
+    scissorsIconP.style.color = "#000000";
+
+    rockIcon.style.color = "#000000";
+    paperIcon.style.color = "#000000";
+    scissorsIcon.style.color = "#000000";
+
+    btnsMoves.forEach((button) => button.disabled = false);
+
+    if (currentRound >= rounds){
+        winner = Number(computerScoreHolder.textContent) > Number(playerScoreHolder.textContent) ? 
+        "computer" : Number(computerScoreHolder.textContent) === Number(playerScoreHolder.textContent)?
+        "draw" : playerName;
+        endMatch(winner);
+        
+    }
+    else if (currentRound < rounds && player !== computer){
+        currentRound++;
+        roundCounter.textContent = currentRound + "/" + rounds;
+    }
+    drawText.textContent = "";       
+
 }
 
-function startMatch(){
-    rounds = Number(inputRounds.value);
-    playerName.textContent = inputName.value;
-    roundCounter.textContent = "1/" + rounds;
 
-    currentRound = 1;
-
-}
 
 async function endMatch(winner){
-    //winner = playerName;
+    btnsMoves.forEach((button) => button.disabled = true);
+
     if (winner === playerName){
-        divWinner.style.display = "flex";
+        WinnerText.textContent = "Winner";
         divWinner.style.justifyContent = "flex-end";
     }
     else if (winner === "draw"){
         WinnerText.textContent = "Draw";
-        divWinner.style.display = "flex";
         divWinner.style.justifyContent = "center";
     }
     else if (winner === "computer"){
-        divWinner.style.display = "flex";
+        WinnerText.textContent = "Winner";
         divWinner.style.justifyContent = "flex-start";
     }
 
     await sleep(3000);
     roundCounter.textContent = "0/0";
 
-    inputName.setAttribute("value", playerName);
+    inputName.setAttribute("value", playerName.textContent);
     inputRounds.setAttribute("value", rounds);
-    modal.style.display = "flex";
+    modal.style.visibility = "visible";
     compScore.textContent = 0;
     playerScore.textContent = 0;
 
@@ -129,11 +169,17 @@ async function endMatch(winner){
 
 
 btnStartMatch.addEventListener("click", () => {
-    startMatch();
-    modal.style.display = "none";
-    divWinner.style.display = "none";
+    modal.style.visibility = "hidden";
+    WinnerText.textContent = "";
+    rounds = Number(inputRounds.value);
+    playerName.textContent = inputName.value;
+    roundCounter.textContent = "1/" + rounds;
+    currentRound = 1;
+    game.style.visibility = "visible";
+
+    btnsMoves.forEach((button) => button.disabled = false);
 })
 
 btnsMoves.forEach((button) => {
-    button.addEventListener("click", () => play(button.textContent.toLowerCase(), compScore, playerScore))
+    button.addEventListener("click", () => play(button.id.toLowerCase(), compScore, playerScore))
 })
